9b12df1a8fe67eeb3963c766d5a2a6b4b4647715
Kareem Farrag <karimaabdullah30@gmail.com>
2023-05-13 15:01:39 +0200

    assignment4-pt2: update assignment commit id

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -6,7 +6,7 @@
 ##############################################################
 
 # Fill up the contents below in order to reference your assignment 3 git contents
-AESD_ASSIGNMENTS_VERSION = 'cbb59f8'
+AESD_ASSIGNMENTS_VERSION = 'fda2c2441d2688e158cb8c4b838fd51160ff1694'
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
